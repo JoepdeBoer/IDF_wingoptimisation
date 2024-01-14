716f0537cc869e1d805c53508c65752fdd38b854
Joep <Joepdeboer-3@student.tudelft.nl>
2024-01-14 16:11:37 +0100

    put constrains as used in the runs ( i laer experimented a bit more)

--- postprocessing/effectofviolation.m
+++ postprocessing/effectofviolation.m
@@ -24,20 +24,20 @@ CO2 = zeros(0,n)'; %empty list for normalised kg CO2
 
 for i = 1:n
     LD = LDlist(i) * 16.3 ; %denormalising
-    CO2(i) = (exp(constant.R*LD^(-1)*C_T/V)/0.938-1)*(constant.W_aw+x(21)) * 3.16 / (5.753 * 10^4); % normalised to ref CO2 
+    CO2(i) = (exp(constant.R*LD^(-1)*C_T/V)/0.938-1)*(constant.W_aw+x(21)) * 3.16 / (5.255 * 10^4); % normalised to ref CO2 
 end
 
 computedLD  = ld *16.3
-computedCO2 = (exp(constant.R*computedLD^(-1)*C_T/V)/0.938-1)*(constant.W_aw+x(21)) * 3.16 / (5.753 * 10^4); % normalised to ref CO2
+computedCO2 = (exp(constant.R*computedLD^(-1)*C_T/V)/0.938-1)*(constant.W_aw+x(21)) * 3.16 / (5.255 * 10^4); % normalised to ref CO2
 
 actualLD = (xsol(19) - output.constrviolation) * 16.3;
-actualCO2 = (exp(constant.R*actualLD^(-1)*C_T/V)/0.938-1)*(constant.W_aw+x(21)) * 3.16 / (5.753 * 10^4); % normalised to ref CO2
+actualCO2 = (exp(constant.R*actualLD^(-1)*C_T/V)/0.938-1)*(constant.W_aw+x(21)) * 3.16 / (5.255 * 10^4); % normalised to ref CO2
 
 figure 
 hold on 
-plot(LDlist , CO2);
-scatter(ld, computedCO2);
-scatter(actualLD/16.3,actualCO2)
+plot(LDlist/xsol(19) , CO2, LineWidth= 2);
+scatter(1, computedCO2, 50, 'filled');
+scatter(actualLD/16.3/xsol(19),actualCO2, 50, 'filled')
 xlabel('Normalised L/D')
 ylabel('Normalised CO2')
 legend('', 'solution', 'actual')
